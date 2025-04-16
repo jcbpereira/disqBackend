e73f3fe7e4277cc3c515ff03b795b6df2400febe
jcbpereira <63594204+jcbpereira@users.noreply.github.com>
2025-04-16 18:42:08 +0100

    add rolling average calculation

--- disqdb/views.py
+++ disqdb/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from .serializers import *
 from .models import Pitch, TestRecord
+from .posture_buffer import add_pitch_measurement, get_current_average
 
 @api_view(['GET', 'POST'])
 def add_pitch_reading(request):
@@ -11,8 +12,11 @@ def add_pitch_reading(request):
     if request.method == 'POST':
         serializer = readingSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
+            instance = serializer.save()
+            # Add to rolling average buffer
+            avg=add_pitch_measurement(instance.value)
+            # Also return rolling average in the response
+            return Response({"average": round(avg, 0)},status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
